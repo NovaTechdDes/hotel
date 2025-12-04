3a934071e5cd8ef8d87f0e6b28041c56a12bb842
Pelusa-not <agustinlorenzatto@outlook.com>
2025-12-04 10:13:28 -0300

    Empezando a realizar el diseño de modo oscuro a modo claro en los clientes

--- src/components/dashboard/cliente/ClienteCard.tsx
+++ src/components/dashboard/cliente/ClienteCard.tsx
@@ -40,24 +40,24 @@ const ClienteCard = ({ cliente }: Props) => {
   if (!nombre.toUpperCase().startsWith(filtro?.toUpperCase() || '') && !dni.toUpperCase().startsWith(filtro?.toUpperCase() || '')) return null;
 
   return (
-    <div className="text-center border text-black bg-white rounded-lg border-gray-300 text-lg p-5">
+    <div className="text-center border text-black bg-white rounded-lg border-gray-300 text-lg p-5 dark:bg-slate-800 dark:text-white dark:border-gray-600">
       <div>
-        <h2 className="font-bold capitalize">{nombre}</h2>
+        <h2 className="font-bold capitalize dark:text-white">{nombre}</h2>
       </div>
 
       <div className="flex flex-col gap-3">
         <div className="flex items-center gap-2">
-          <LuIdCard className="text-gray-600" />
-          <p className="capitalize text-gray-800 text-sm">{dni}</p>
+          <LuIdCard className="text-gray-600 dark:text-gray-400" />
+          <p className="capitalize text-gray-800 dark:text-white">{dni}</p>
         </div>
         <div className="flex items-center gap-2">
-          <LuPhone className="text-gray-600" />
-          <p className="capitalize text-gray-800 text-sm">{telefono}</p>
+          <LuPhone className="text-gray-600 dark:text-gray-400" />
+          <p className="capitalize text-gray-800 dark:text-white">{telefono}</p>
         </div>
         {localidad && (
           <div className="flex items-center gap-2">
-            <CiLocationOn className="text-gray-600" />
-            <p className="capitalize text-gray-800 text-sm">{localidad}</p>
+            <CiLocationOn className="text-gray-600 dark:text-gray-400" />
+            <p className="capitalize text-gray-800 dark:text-white">{localidad}</p>
           </div>
         )}
       </div>
@@ -65,20 +65,23 @@ const ClienteCard = ({ cliente }: Props) => {
       <div className="flex items-center gap-2 justify-center  my-2">
         {isPending ? (
           <span className="flex items-center gap-1 text-gray-500">
-            <span className="w-4 h-4 border-2 border-gray-400 border-t-black rounded-full animate-spin"></span>
-            <span className="text-xs">Cargando...</span>
+            <span className="w-6 h-6 border-2 border-gray-400 border-t-black dark:border-t-white rounded-full animate-spin"></span>
+            <span className="text-sm dark:text-white">Cargando...</span>
           </span>
         ) : (
           <div className="grid grid-cols-2 w-full mt-10  gap-2">
             <button
-              className="flex items-center gap-2 text-white-500 justify-center hover:bg-gray-200 bg-white border border-gray-300 text-black cursor-pointer p-2 rounded-lg"
+              className="flex items-center gap-2 text-white-500 justify-center hover:bg-gray-200 bg-white border border-gray-300 text-black cursor-pointer p-2 rounded-lg dark:bg-slate-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700"
               onClick={handleUpdateCliente}
             >
-              <BiPencil size={20} className="cursor-pointer hover:bg-gray-300 rounded-lg" />
+              <BiPencil size={20} className="cursor-pointer rounded-lg" />
               <p>Editar</p>
             </button>
             {user && user.rol === 'admin' && (
-              <button className="flex items-center justify-center gap-2 text-white-500 hover:bg-red-500 bg-red-600 text-white cursor-pointer p-2 rounded-lg" onClick={handleDeleteCliente}>
+              <button
+                className="flex items-center justify-center gap-2 text-white-500 hover:bg-red-500 bg-red-600 text-white cursor-pointer p-2 rounded-lg dark:bg-slate-800 dark:text-red-500 dark:border-red-500 dark:border dark:hover:bg-gray-700"
+                onClick={handleDeleteCliente}
+              >
                 <MdDeleteOutline className=" cursor-pointer rounded-lg" size={20} />
                 <p>Eliminar</p>
               </button>
--- src/pages/dashboard/Cliente.tsx
+++ src/pages/dashboard/Cliente.tsx
@@ -1,4 +1,3 @@
-
 import ListaClientes from '../../components/dashboard/cliente/ListaClientes';
 import { ModalCliente } from '../../components/dashboard/cliente/ModalCliente';
 
@@ -6,24 +5,25 @@ import { useClienteStore } from '../../store/cliente.store';
 import { HeaderMain } from '../../components';
 
 export const Cliente = () => {
-    const { isModalOpen, openModal, setFiltro } = useClienteStore();
+  const { isModalOpen, openModal, setFiltro } = useClienteStore();
 
-    return (
-        <main>
-            <HeaderMain openModal={openModal} botonText='Agregar Cliente' />
+  return (
+    <main>
+      <HeaderMain openModal={openModal} botonText="Agregar Cliente" />
 
-            <div className='text-black mx-5 my-2'>
-                <input 
-                 type="text"
-                 onChange={(e) => setFiltro(e.target.value)}
-                 name="buscador" id="buscador"
-                 placeholder='Buscar cliente por nombre o DNI'
-                 className='placeholder:text-slate-500 w-full p-2 border border-slate-300 bg-white rounded-md focus:outline-none focus:border-sky-500'
-                 />
-            </div>
+      <div className="text-black mx-5 my-2">
+        <input
+          type="text"
+          onChange={(e) => setFiltro(e.target.value)}
+          name="buscador"
+          id="buscador"
+          placeholder="Buscar cliente por nombre o DNI"
+          className="placeholder:text-slate-500 w-full p-2 border border-slate-300 bg-white rounded-md focus:outline-none focus:border-sky-500 dark:bg-slate-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700"
+        />
+      </div>
 
-            <ListaClientes />
-            {isModalOpen && <ModalCliente />}
-        </main>
-    )
-}
+      <ListaClientes />
+      {isModalOpen && <ModalCliente />}
+    </main>
+  );
+};
